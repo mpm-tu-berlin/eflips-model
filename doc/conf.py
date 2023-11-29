d6ecd56b95fda893c70b13a48561833aba816c0f
Ludger Heide <ludger.heide@tu-berlin.de>
2023-11-29 13:50:28 +0100

    Start work on database

---
+++ doc/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "eFLIPS-DB"
+copyright = "2023, Technische Universität Berlin"
+author = "Ludger Heide"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "autoapi.extension", "sphinx_paramlinks"]
+autodoc_typehints = "description"
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for autoapi -----------------------------------------------------
+autoapi_dirs = ["../eflips/"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
