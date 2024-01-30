89eba13f0dcf10154e5f8681d3f81b66e01fa3d0
Ludger Heide <ludger.heide@tu-berlin.de>
2024-01-30 12:49:11 +0100

    Bump version to 1.1.6

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-model"
 copyright = "2023, Technische Universität Berlin"
 author = "Ludger Heide"
-release = "1.1.5"
+release = "1.1.6"
 
 
 # -- General configuration ---------------------------------------------------
