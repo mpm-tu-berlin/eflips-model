83be257ff245dd1878a9fd87ea05a145a61fc577
Ludger Heide <ludger.heide@tu-berlin.de>
2024-03-18 17:29:22 +0100

    Remove unique constraint on `Process.name`

--- eflips/model/depot.py
+++ eflips/model/depot.py
@@ -208,7 +208,6 @@ class Process(Base):
 
     name: Mapped[str] = mapped_column(Text)
     """A name for the process. If set, it must be unique within the scenario."""
-    _table_args_list.append(UniqueConstraint(scenario_id, name))
 
     name_short: Mapped[str] = mapped_column(Text, nullable=True)
     """An optional short name for the process."""
