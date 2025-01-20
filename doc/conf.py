a94684d7a329cbae34066d56758792a33caa6d96
Ludger Heide <ludger.heide@tu-berlin.de>
2025-01-20 15:11:28 +0100

    ALso allow STANDBY events at stations.

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-model"
 copyright = "2024, Technische Universität Berlin"
 author = "Ludger Heide"
-release = "6.0.3"
+release = "6.0.4"
 
 
 # -- General configuration ---------------------------------------------------
