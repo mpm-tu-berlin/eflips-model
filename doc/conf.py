df07e3f8a7bf5139a539828d6f05e0489718c5d8
Shuyao Guo <shuyao.guo@tu-berlin.de>
2025-05-06 19:02:21 +0200

    Version 7.1.0 with migration script

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-model"
 copyright = "2024, Technische Universität Berlin"
 author = "Ludger Heide"
-release = "7.0.3"
+release = "7.1.0"
 
 
 # -- General configuration ---------------------------------------------------
