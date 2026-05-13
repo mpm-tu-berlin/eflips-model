d15ee0add97dbcefc6fdf10c10be2a2de19227a6
Shuyao Guo <shuyao.guo@tu-berlin.de>
2026-05-13 17:33:53 +0200

    Version 11.2.0

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-model"
 copyright = "2024, Technische Universität Berlin"
 author = "Ludger Heide"
-release = "11.1.0"
+release = "11.2.0"
 
 
 # -- General configuration ---------------------------------------------------
